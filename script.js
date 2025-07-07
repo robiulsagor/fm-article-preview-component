5df13fe51b7e9531fcac06c90fe4b1aa785fbd7d
robiulsagor <robiul100.me@gmail.com>
2025-07-07 22:49:06 +0600

    feat: Desktop screen design is also complete

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const shareBtn = document.getElementById("card-share-button")
 const shareMenu = document.getElementById("card-share")
+const card = document.querySelector(".card")
 
 let isShareMenuOpen = false;
 
 const toggleShareMenu = () => {
  isShareMenuOpen = !isShareMenuOpen;
  shareMenu.classList.toggle("show", isShareMenuOpen);
+ card.style.overflow = isShareMenuOpen ? "visible" : "hidden";
 }
 
 shareBtn.addEventListener("click", toggleShareMenu)
